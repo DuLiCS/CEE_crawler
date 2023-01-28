b2e61c27e8cd39c175ba8b50eccc4c1d8791f477
Li Du <ieee811@gmail.com>
2023-01-29 00:24:41 +0800

    score crawler complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_scrape import *
 
-data_scrape = DataScrape(school_name='上海交通大学')
+data_scrape = DataScrape(school_name='南京审计大学')
+data_scrape.special_score_scrape()
 data_scrape.special_plan_scrape()
 # data_scrape.province_score_scrape()
